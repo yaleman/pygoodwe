374ce30534fbc808cda0de4f8db5fc636fa3c0cc
James Hodgkinson <yaleman@ricetek.net>
2019-11-03 15:51:20 +1000

    doing some testing

--- test.py
+++ test.py
@@ -9,18 +9,20 @@ gw = SingleInverter(
         account=args.get('gw_account', 'thiswillnotwork'), 
         password=args.get('gw_password', 'thiswillnotwork'),
         )
-print("Grabbing data")
+# print("Grabbing data")
 gw.getCurrentReadings(raw=True)
 
 # battery state of charge
-print(f"Current SOC: {gw.get_battery_soc()}")
+# print(f"Current SOC: {gw.get_battery_soc()}")
+
+print(json.dumps(gw.data, indent=2))
 
 #print(json.dumps(gw.data))
-print(f"Are the batteries full? {gw.are_batteries_full(fullstate=90.0)}")
+#print(f"Are the batteries full? {gw.are_batteries_full(fullstate=90.0)}")
 
 #print(gw.data.keys())
 #print(json.dumps(gw.data['inverter']))
 #print(json.dumps(gw.data))
 
-print(f"PV Flow: {gw.getPVFlow()}")
-print(f"Voltage: {gw.getVoltage()}")
+# print(f"PV Flow: {gw.getPVFlow()}")
+# print(f"Voltage: {gw.getVoltage()}")
