f7f8de0f3b0381b62bc8eac7bc2d8ec0261f5c88
James Hodgkinson <james@terminaloutcomes.com>
2023-03-20 10:35:59 +1000

    adding simple dump example

---
+++ examples/dump.py
@@ -0,0 +1,18 @@
+""" dumps the raw JSON response from the API """
+import json
+
+from config import args
+from pygoodwe import  API
+
+def main():
+    """ dumps the raw data """
+    goodwe = API(
+            system_id=args.get('gw_station_id', '1'),
+            account=args.get('gw_account', 'thiswillnotwork'),
+            password=args.get('gw_password', 'thiswillnotwork'),
+            )
+    goodwe.getCurrentReadings()
+    print(json.dumps(goodwe.data, indent=4))
+
+if __name__ == "__main__":
+    main()
